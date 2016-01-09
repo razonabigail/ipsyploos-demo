c00ab496d4d4f6552e335244eb6f7c85ebd2813c
Abigail Razon <razonabigail@gmail.com>
2016-01-09 07:41:04 +0000

    Edited processing controller

--- app/controllers/processing_controller.rb
+++ app/controllers/processing_controller.rb
@@ -3,8 +3,16 @@ class ProcessingController < ApplicationController
   	shop = params[:shop]
   	creative_id = params[:creative_id]
   	user_id = current_user.id
+
+  	# UNCOMMENT WHEN DUMMY SHOP SITES ARE ACTIVE
+  	#url = 'http://strk.cakemarketing.com/?a=#{user_id}&c=#{creative_id}&p=f&s1='
+	#exec "curl #{url}"
+	#exec "curl http://www.#{shop}.com/processing?url=#{url}"
+
   	#exec "curl http://strk.cakemarketing.com/?a=#{user_id}&c=#{creative_id}&p=f&s1="
   	#exec "curl http://strk.cakemarketing.com/?a=5755&c=1442&p=f&s1=Sample+Product_ID&s2=Sample+Price"
+  	
+
   	redirect_to "http://www.#{shop}.com"
   end
 end
